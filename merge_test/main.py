528a537fbf757c53a3177c0f78200716be210cb6
Dario Abbondanza <43699847+dario-abbondanza@users.noreply.github.com>
2025-02-20 18:16:08 +0000

    Second commit on master

--- merge_test/main.py
+++ merge_test/main.py
@@ -1,6 +1,9 @@
-from utils import add
+from utils import add, multiply
 
 
 def main():
     print("You are on Master Branch ")
+    result = multiply(80.5, 33)
+    print(f"The result of multiplication is {result}")
 
+main()
