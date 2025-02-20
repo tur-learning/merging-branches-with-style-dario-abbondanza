3b4e752925ad83e5b4368a5c6ee8982ae64030b4
Dario Abbondanza <43699847+dario-abbondanza@users.noreply.github.com>
2025-02-20 18:09:57 +0000

    calling add function from the main

--- merge_test/main.py
+++ merge_test/main.py
@@ -2,5 +2,11 @@ from utils import add
 
 
 def main():
-    print("You are on Master Branch ")
+    print("You are on feature a Branch ")
+    a = 10
+    b = 50
+    result = add(a, b)
+    print(f"The result of the addition is {result}")
 
+
+main()
--- merge_test/utils.py
+++ merge_test/utils.py
@@ -2,4 +2,8 @@ def add(a, b):
     res = a + b
     return res
 
-print("You are into utils.py")
+
+if __name__ == "__main__":
+    print("You are into utils.py")
+    result = add(10, 20)
+    print(result)
